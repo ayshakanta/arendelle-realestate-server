030f7b41ea7d0f18c43f97847f230d40806a8540
ayshakanta <ayshahussain291@gmail.com>
2024-06-22 21:35:10 +0600

    added vercel

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+     
+    ]
+  })
+);
 
-
-const corsOptions = {
-  origin: ['http://localhost:5173'],
-  credentials: true,
-  optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: ['http://localhost:5173'],
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// }
+// app.use(cors(corsOptions))
 
 
 app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ronnby7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,6 +46,14 @@ async function run() {
 
     app.get('/properties', async(req, res) =>{
       const result = await propertyCollection.find().toArray()
+      res.send(result)
+    })
+
+
+    app.get('/property/:id', async(req, res) =>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await propertyCollection.findOne(query)
       res.send(result)
     })
 
