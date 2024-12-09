46a1fea33b80902096a9361e028aebc6df73a34d
ayshakanta <ayshahussain291@gmail.com>
2024-12-10 03:11:35 +0600

    added delete

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/wishlist/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await wishlistCollection.deleteOne(query);
+      res.send(result);
+    })
+
     
 
 
