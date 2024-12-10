1a29b9265b01ee297102c04ada1bc881afb0f1c5
ayshakanta <ayshahussain291@gmail.com>
2024-12-10 12:36:44 +0600

    get user data

--- index.js
+++ index.js
@@ -40,8 +40,22 @@ async function run() {
 
 
     // user related api
+
+    app.get("/users", async(req, res)=>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+
     app.post("/users", async(req, res)=>{
       const user = req.body;
+
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: "User already exist", insertedId: null})
+      }
+
       const result = await userCollection.insertOne(user);
       res.send(result)
     })
