4ffc5d4e9816f2fed6a86fab2b9c4ad761e3f753
ayshakanta <ayshahussain291@gmail.com>
2024-12-10 23:46:54 +0600

    made admin

--- index.js
+++ index.js
@@ -60,6 +60,42 @@ async function run() {
       res.send(result)
     })
 
+    // make admin 
+
+    app.patch("/users/admin/:id", async(req, res)=>{
+      const id = req.params.id 
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc ={
+        $set: {
+          role: "admin"
+        }
+      }
+      const result = await userCollection.updateOne(filter,  updatedDoc)
+      res.send(result)
+    })
+
+    // make agent 
+
+    app.patch("/users/agent/:id", async(req, res)=>{
+      const id = req.params.id 
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc ={
+        $set: {
+          role: "agent"
+        }
+      }
+      const result = await userCollection.updateOne(filter,  updatedDoc)
+      res.send(result)
+    })
+
+
+    app.delete("/users/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
     app.get('/properties', async(req, res) =>{
       const result = await propertyCollection.find().toArray()
       res.send(result)
